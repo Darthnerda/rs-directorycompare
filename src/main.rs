b54a230a4275b550caa628adcdfbe648c1792f64
Josh Bevan <josh.bevan@lfest.org>
2024-02-16 02:30:45 -0800

    First productively useful build. Added file nested copying.

--- src/main.rs
+++ src/main.rs
@@ -26,17 +26,34 @@ fn main() -> Result<(), eframe::Error> {
     )
 }
 
+#[derive(Clone)]
+struct FileInfo {
+    filename: OsString,
+    path: PathBuf,
+    should_copy: bool,
+    size: u64
+}
+
+struct CompInfo {
+    left: Vec<FileInfo>,
+    right: Vec<FileInfo>,
+    left_path: PathBuf,
+    right_path: PathBuf
+}
+
 struct MyApp {
     dir1Path: Option<PathBuf>,
     dir2Path: Option<PathBuf>,
     dir1_save_path: Option<PathBuf>,
     dir2_save_path: Option<PathBuf>,
     working: bool,
-    result: Option<(Vec<(OsString, bool)>, Vec<(OsString, bool)>, HashMap<OsString, PathBuf>, HashMap<OsString, PathBuf>)>,
-    sender: mpsc::Sender<std::io::Result<(Vec<OsString>, Vec<OsString>, HashMap<OsString, PathBuf>, HashMap<OsString, PathBuf>)>>,
-    receiver: mpsc::Receiver<std::io::Result<(Vec<OsString>, Vec<OsString>, HashMap<OsString, PathBuf>, HashMap<OsString, PathBuf>)>>,
+    copying: bool,
+    result: Option<CompInfo>,
+    sender: mpsc::Sender<std::io::Result<CompInfo>>,
+    receiver: mpsc::Receiver<std::io::Result<CompInfo>>,
     work_thread: Option<std::thread::JoinHandle<()>>,
-    copy_thread: Option<std::thread::JoinHandle<Vec<Option<std::io::Error>>>>,
+    left_copy_thread: Option<std::thread::JoinHandle<Vec<Option<std::io::Error>>>>,
+    right_copy_thread: Option<std::thread::JoinHandle<Vec<Option<std::io::Error>>>>,
     dir1_entries_selected: Selected,
     dir2_entries_selected: Selected
 }
@@ -62,11 +79,13 @@ impl Default for MyApp {
             dir1_save_path: None,
             dir2_save_path: None,
             working: false,
+            copying: false,
             result: None,
             sender: sender,
             receiver: receiver,
             work_thread: None,
-            copy_thread: None,
+            left_copy_thread: None,
+            right_copy_thread: None,
             dir1_entries_selected: Selected::new(false, false),
             dir2_entries_selected: Selected::new(false, false)
         }
@@ -117,7 +136,7 @@ impl eframe::App for MyApp {
                     let dir2 = self.dir2Path.as_mut().unwrap().as_mut_os_string().to_owned();
 
                     self.work_thread = Some(thread::spawn(move || {
-                        let result: Result<(Vec<OsString>, Vec<OsString>, HashMap<OsString, PathBuf>, HashMap<OsString, PathBuf>), std::io::Error> = tokio::runtime::Builder::new_multi_thread()
+                        let result: Result<CompInfo, std::io::Error> = tokio::runtime::Builder::new_multi_thread()
                             .enable_all()
                             .build()
                             .unwrap()
@@ -133,10 +152,8 @@ impl eframe::App for MyApp {
                 Err(mpsc::TryRecvError::Disconnected) => Err("Error: Work thread disconnected. Probably crashed. Ask Josh.".to_string()),
                 Ok(r) => match r {
                     Err(e) => Err(e.to_string()),
-                    Ok((lefts, rights, left_path_map, right_path_map)) => Ok({
-                        let lv: Vec<(OsString, bool)> = lefts.into_iter().map(|st| (st, false)).collect();
-                        let rv: Vec<(OsString, bool)> = rights.into_iter().map(|st| (st, false)).collect();
-                        self.result = Some((lv, rv, left_path_map, right_path_map));
+                    Ok(_result) => Ok({
+                        self.result = Some(_result);
                         self.working = false;
                     })
                 }
@@ -150,27 +167,65 @@ impl eframe::App for MyApp {
                 ui.label("Working on it...");
             }
 
+            if self.copying {
+                ui.label("Copying...");
+            }
+
             if self.result.is_some() {
 
-                let (lefts, rights, left_path_map, right_path_map) = self.result.as_mut().unwrap();
+                // let (lefts, rights, left_path_map, right_path_map) = self.result.as_mut().unwrap();
+                let CompInfo {
+                    left: lefts,
+                    right: rights,
+                    left_path: _left_path,
+                    right_path: _right_path
+                } = self.result.as_mut().unwrap();
 
-                if self.dir1_save_path.is_some() || self.dir2_save_path.is_some() {
+                if !self.copying && self.dir1_save_path.is_some() || self.dir2_save_path.is_some() {
                     if ui.button("Begin copy of missing files.").clicked() {
                         if self.dir1_save_path.is_some() {
                             let target_dir = self.dir1_save_path.as_ref().unwrap().clone();
                             let right = rights.clone();
-                            let path_map = left_path_map.clone();
-                            // self.copy_thread = Some(thread::spawn(move || do_copy(right, path_map, target_dir)));
-                            thread::spawn(move || do_copy(right, path_map, target_dir));
+                            let right_path = _right_path.clone();
+                            self.left_copy_thread = Some(thread::spawn(move || do_copy(right, right_path.clone(), target_dir)));
+                            self.copying = true;
                         }
                         if self.dir2_save_path.is_some() {
                             let target_dir = self.dir2_save_path.as_ref().unwrap().clone();
                             let left = lefts.clone();
-                            let path_map = left_path_map.clone();
-                            // self.copy_thread = Some(thread::spawn(move || do_copy(left, path_map, target_dir)));
-                            thread::spawn(move || do_copy(left, path_map, target_dir));
+                            let left_path = _left_path.clone();
+                            self.right_copy_thread = Some(thread::spawn(move || do_copy(left, left_path.clone(), target_dir)));
+                            self.copying = true;
                         }
                     }
+                }
+
+                if self.left_copy_thread.is_some() && self.left_copy_thread.as_ref().unwrap().is_finished() {
+                    match self.left_copy_thread.take().unwrap().join() {
+                        Ok(potential_errors) => {
+                            potential_errors.into_iter().filter_map(|possible_error| possible_error).for_each(|err| println!("{:?}", err));
+                            ui.label("");
+                        },
+                        Err(_) => {
+                            ui.label("Error: Something went wrong with the left copy thread when joining. Ask Josh.");
+                        }
+                    }
+                }
+
+                if self.right_copy_thread.is_some() && self.right_copy_thread.as_ref().unwrap().is_finished() {
+                    match self.right_copy_thread.take().unwrap().join() {
+                        Ok(potential_errors) => {
+                            potential_errors.into_iter().filter_map(|possible_error| possible_error).for_each(|err| println!("{:?}", err));
+                            ui.label("");
+                        },
+                        Err(_) => {
+                            ui.label("Error: Something went wrong with the left copy thread when joining. Ask Josh.");
+                        }
+                    }
+                }
+
+                if self.left_copy_thread.is_none() && self.right_copy_thread.is_none() {
+                    self.copying = false;
                 }
 
                 TableBuilder::new(ui)
@@ -180,27 +235,27 @@ impl eframe::App for MyApp {
                     .header(20.0, |mut header| {
                         header.col(|ui| {
                             select_all_checkbox(ui, lefts, &mut self.dir1_entries_selected, "Directory 1");
-                            if !self.working && ui.button("Set destination for files missing from directory 1.").clicked() {
+                            if !self.copying && !self.working && ui.button("Set destination for files missing from directory 1.").clicked() {
 
                                 self.dir1_save_path = FileDialog::new()
                                     .set_title("Choose where files should be copied.")
                                     .pick_folder();
                             }
                             if self.dir1_save_path.is_some() {
-                                ui.label(format!("Directory 2: {:?}", self.dir1_save_path.as_ref().unwrap()));
+                                ui.label(format!("Destination: {:?}", self.dir1_save_path.as_ref().unwrap()));
                             }
                             // ui.heading("Directory 1");
                         });
                         header.col(|ui| {
                             select_all_checkbox(ui, rights, &mut self.dir2_entries_selected, "Directory 2");
-                            if !self.working && ui.button("Set destination for files missing from directory 2.").clicked() {
+                            if !self.copying && !self.working && ui.button("Set destination for files missing from directory 2.").clicked() {
 
                                 self.dir2_save_path = FileDialog::new()
                                     .set_title("Choose where files should be copied.")
                                     .pick_folder();
                             }
                             if self.dir2_save_path.is_some() {
-                                ui.label(format!("Directory 2: {:?}", self.dir2_save_path.as_ref().unwrap()));
+                                ui.label(format!("Destination: {:?}", self.dir2_save_path.as_ref().unwrap()));
                             }
                             // ui.heading("Directory 2");
                         });
@@ -211,16 +266,16 @@ impl eframe::App for MyApp {
                                 
                                 row.col(|ui| {
                                     if i < lefts.len() {
-                                        let (filename, should_transfer) = &mut lefts[i];
-                                        ui.checkbox(should_transfer, filename.to_string_lossy()).handle_checkbox_change(&mut self.dir1_entries_selected);
+                                        let FileInfo { filename, should_copy, .. } = &mut lefts[i];
+                                        ui.checkbox(should_copy, filename.to_string_lossy()).handle_checkbox_change(&mut self.dir1_entries_selected);
                                     }
                                 });
                                 
                                 
                                 row.col(|ui| {
                                     if i < rights.len() {
-                                        let (filename, should_transfer) = &mut rights[i];
-                                        ui.checkbox(should_transfer, filename.to_string_lossy()).handle_checkbox_change(&mut self.dir2_entries_selected);
+                                        let FileInfo { filename, should_copy, .. } = &mut rights[i];
+                                        ui.checkbox(should_copy, filename.to_string_lossy()).handle_checkbox_change(&mut self.dir2_entries_selected);
                                     }
                                 });
                             });
@@ -231,24 +286,19 @@ impl eframe::App for MyApp {
     }
 }
 
-fn do_copy(to_copy: Vec<(OsString, bool)>, path_map: HashMap<OsString, PathBuf>, target_dir: PathBuf) -> Vec<Option<std::io::Error>> {
+fn do_copy(to_copy: Vec<FileInfo>, source_dir: PathBuf, target_dir: PathBuf) -> Vec<Option<std::io::Error>> {
     to_copy.iter()
-        .filter(| (_, should_copy) | *should_copy)
-        .map(| (filename, _) | {
-            let source_path = path_map.get(filename).expect("Error: Path map does not exhaustively contain all filenames. Ask Josh.");
-            let target_path = Path::new(&target_dir).join(filename);
+        .filter(| FileInfo { should_copy, .. } | *should_copy)
+        .map(| FileInfo { filename, path: source_path, ..} | {
+            let target_path = Path::new(&target_dir)
+                .join(source_path.strip_prefix(&source_dir).expect("Error: Path of source file not in source directory. Ask Josh"));
             // println!("{}, {}", source_path.display(), target_path.display());
+            // Create all parent directories of the destination path
+            if let Some(parent_dir) = target_path.parent() {
+                std::fs::create_dir_all(parent_dir)?;
+            }
             std::fs::copy(source_path, target_path)
         })
-        // .map(| (filename, _) | path_map.get(filename).expect("Error: Path map does not exhaustively contain all filenames. Ask Josh.") )
-        // .map(| source_path | {
-        //     let mut target_path = PathBuf::from(&target_dir);
-        //     target_path.push(&source_path);
-        //     // if let Ok(suffix) = source_path.strip_prefix(&dpath) {
-        //     // } else {}
-        //     println!("{}, {}", source_path.display(), target_path.display());
-        //     // std::fs::copy(source_path, target_path)
-        // })
         .map(| result | result.err())
         .collect()
 }
@@ -266,31 +316,50 @@ impl Handleable for egui::Response {
     }
 }
 
-fn select_all_checkbox(ui: &mut egui::Ui, all: &mut Vec<(OsString, bool)>, selected: &mut Selected, text: impl Into<egui::WidgetText>) {
+fn select_all_checkbox(ui: &mut egui::Ui, all: &mut Vec<FileInfo>, selected: &mut Selected, text: impl Into<egui::WidgetText>) {
     let checkbox = egui::Checkbox::new(&mut selected.bool, text).indeterminate(selected.indeterminate);
     if ui.add(checkbox).changed() {
-        all.iter_mut().for_each(| (_, checked) | *checked = selected.bool);
+        all.iter_mut().for_each(| FileInfo {should_copy: checked, ..} | *checked = selected.bool);
         selected.indeterminate = false;
     }
 }
 
 
-async fn find_diffs(dir1: &OsString, dir2: &OsString) -> std::io::Result<(Vec<OsString>, Vec<OsString>, HashMap<OsString, PathBuf>, HashMap<OsString, PathBuf>)> {
+async fn find_diffs(dir1: &OsString, dir2: &OsString) -> std::io::Result<CompInfo> {
     let mut names1: HashSet<(OsString, u64)> = HashSet::with_capacity(10000);
     let mut names2: HashSet<(OsString, u64)> = HashSet::with_capacity(10000);
 
-    let mut left_path_map: HashMap<OsString, PathBuf> = HashMap::with_capacity(10000);
-    let mut right_path_map: HashMap<OsString, PathBuf> = HashMap::with_capacity(10000);
+    let mut _left_path_map: HashMap<OsString, PathBuf> = HashMap::with_capacity(10000);
+    let mut _right_path_map: HashMap<OsString, PathBuf> = HashMap::with_capacity(10000);
 
-    let fut1 = collect_filenames(dir1, &mut names1, &mut left_path_map);
-    let fut2 = collect_filenames(dir2, &mut names2, &mut right_path_map);
+    let fut1 = collect_filenames(dir1, &mut names1, &mut _left_path_map);
+    let fut2 = collect_filenames(dir2, &mut names2, &mut _right_path_map);
 
     let _ = tokio::join!(fut1, fut2);
 
-    let lefts: Vec<OsString> = names1.difference(&names2).map(|(nm, _)| nm.to_owned()).collect();
-    let rights: Vec<OsString> = names2.difference(&names1).map(|(nm, _)| nm.to_owned()).collect();
+    let _lefts: Vec<FileInfo> = names1.difference(&names2).map(|(nm, sz)| 
+        FileInfo {
+            path: _left_path_map.get(nm).expect("Error: Path map does not exhaustively contain all filenames. Ask Josh.").to_owned(),
+            filename: nm.to_owned(), 
+            should_copy: false, 
+            size: sz.to_owned()
+        }
+    ).collect();
+    let _rights: Vec<FileInfo> = names2.difference(&names1).map(|(nm, sz)| 
+        FileInfo {
+            path: _right_path_map.get(nm).expect("Error: Path map does not exhaustively contain all filenames. Ask Josh.").to_owned(),
+            filename: nm.to_owned(), 
+            should_copy: false, 
+            size: sz.to_owned()
+        }
+    ).collect();
 
-    Ok((lefts, rights, left_path_map, right_path_map))
+    Ok(CompInfo {
+        left: _lefts,
+        right: _rights,
+        left_path: PathBuf::from(dir1),
+        right_path: PathBuf::from(dir2)
+    })
 }
 
 #[async_recursion(?Send)]
